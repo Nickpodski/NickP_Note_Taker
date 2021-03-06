c6f6a1341bd66007a371826b979b6f6e19ed6370
Nickpodski <Nickpodski@gmail.com>
2021-03-06 03:07:06 -0500

    changed a few things

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,19 @@
-const notes = require('../db/db.json');
+const Notes = require('../lib/Notes.js');
+let storedNotes = new Notes;
 
 module.exports = (app) => {
-  app.get('/api/notes', (req, res) => res.json(notes));
+  app.get("/api/notes", (req, res) => {
+    res.send(storedNotes.getNotes());
+  })
 
+  app.post("/api/notes", (req, res) => {
+    storedNotes.addNote(req.body);
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+  })
 
-  app.post('/api/notes', (req, res) => {
-      notes.push(req.body);
-      res.json(true);
-  });
-
-  app.post('/api/clear', (req, res) => {
-    notes.length = 0;
-
-    res.json({ ok: true });
-  });
+  app.delete("/api/notes/:id", (req, res) => {
+    const { id } = req.params;
+    storedNotes.delNote(id);
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+  })
 };
